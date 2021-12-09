5b63da85a54a06cb24064d8d9298148df5d36504
Shirley Tatiana Diaz <shirley.diaz.mt@correo.usa.edu.co>
2021-12-08 22:41:12 -0500

    Frontend Reto 3

---
+++ js/url.js
@@ -0,0 +1,8 @@
+//backend julian
+// var url = "http://129.151.121.220:8081"
+//backend samuel
+// var url ="http://152.70.141.56:8082"
+//backend Tatiana
+var url ="http://150.136.87.138:8080"
+//backend local
+//var url ="http://localhost:8082";
